74a5662131cb5ab2247a73c0592b1093eb8a1461
Krishna <krishnakris2007@gmail.com>
2026-03-09 10:54:14 +0530

    Update password to validate new example

--- PasswordValidator.java
+++ PasswordValidator.java
@@ -1,16 +1,15 @@
 class PasswordValidator {
-    public static void main(String[] args) {
-        String pwd = "Abc@1234"; // fixed password to check
-
-        if (pwd.length() >= 8 &&
-            !pwd.contains(" ") &&
-            pwd.matches(".*[A-Z].*") &&
-            pwd.matches(".*[a-z].*") &&
-            pwd.matches(".*[0-9].*") &&
-            pwd.matches(".*[^a-zA-Z0-9].*")) {
-            System.out.println("Password is valid!");
-        } else {
-            System.out.println("Password does not meet requirements.");
-        }
-    }
+public static void main(String[] args) {
+String pwd = "Krish@12"; 
+if (pwd.length() >= 8 &&
+!pwd.contains(" ") &&
+pwd.matches(".*[A-Z].*") &&
+pwd.matches(".*[a-z].*") &&
+pwd.matches(".*[0-9].*") &&
+pwd.matches(".*[^a-zA-Z0-9].*")) {
+System.out.println("Password is valid!");
+}else{
+System.out.println("Password does not meet requirements.");
+}
+}
 }
